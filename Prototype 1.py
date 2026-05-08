cc10ca91d53de859408506399167a9b8b34c1966
Aadit Lamba (12MG) <aadlam0809@highgateschool.org.uk>
2026-05-08 10:01:16 +0100

    Initial commit

---
+++ Prototype 1.py
@@ -0,0 +1,63 @@
+def place_item(item, pos, grid):
+    for i in range(pos[0],pos[0]+item[0]):
+        for j in range(pos[1],pos[1]+item[1]):
+            grid[i][j] = 'X'
+    return grid
+
+def verify_placement(item, pos, grid):
+    if pos[0] + item[0] -1 <= dimensions[0]-1 and pos[1] + item[1] -1 <= dimensions[1]-1:
+        empty = True
+        for i in range(pos[0],pos[0]+item[0]):
+            for j in range(pos[1],pos[1]+item[1]):
+                if grid[i][j] == 'X':
+                    empty = False
+        if empty:
+            return True
+    return False
+
+def place_next_item(grid,items,current,placed_items):
+    row = 0
+    col = 0
+    found = False
+    end = False
+    while not found and not end:
+        if grid[row][col] != 'X':
+            temp_pos = (row,col)
+            result = verify_placement(items[current], temp_pos, grid)
+            if result:
+                found = True
+                grid = place_item(items[current], temp_pos, grid)
+                placed_items.append(items[current])
+        if col < len(grid[0])-1:
+            col += 1
+        elif row == len(grid)-1:
+            end = True
+            print('No space for this item:', items[current])
+        else:
+            row += 1
+            col = 0
+    current += 1
+    if current < len(items):
+        place_next_item(grid, items, current, placed_items)
+    return placed_items
+   
+
+placed_items = []
+
+dimensions = (10,10)
+
+items = [(1,3),
+         (2,5),
+         (3,2),
+         (1,1),
+         (7,7)
+         ]
+
+grid = [['O']*dimensions[1] for i in range(dimensions[0])]
+placed_items = place_next_item(grid, items, 0, placed_items)
+
+#for row in grid:
+#    print(row)
+   
+for row in grid:
+    print('  '.join(f'{col:>3}' for col in row))
